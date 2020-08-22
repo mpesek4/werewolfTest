3ef46d521376cc8300998a4a08ac2ea5f6342144
Michael Pesek <pesekm@dupage.edu>
2020-08-22 12:45:24 -0500

    media stream not rendering

--- src/components/GameRoom.js
+++ src/components/GameRoom.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Peer from "peerjs";
 import { db } from "../firebase/firebase";
+import VideoGrid from  "./VideoGrid"
 
 
 export default function GameRoom(props) {
@@ -15,8 +16,11 @@ export default function GameRoom(props) {
     { userId: "Michael P" },
   ]);
 
+  const [videoList, setVideoList] = useState([]);
   
   const [stream, setStream] = useState(new MediaStream);
+
+  const [ourId, setOurId] = useState("-1");
 
   const [peers, setPeers] = useState({});
 
@@ -32,36 +36,70 @@ export default function GameRoom(props) {
     })
   );
 
- 
-
-  
-
-  
 
   useEffect(() => {
-    // // console.log("MY PEER IS", myPeer);
+    console.log("MY PEER IS", myPeer);
     myVideo.muted = true;
     
-    getStream()
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(stream => {
+      addVideoStream(myVideo, stream)
+    
+      console.log("what is my stream", stream)
+    
+      myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+
+
+        call.on('stream', userVideoStream => {
+          console.log("how many times am I happening, stream: ", userVideoStream)
+          addVideoStream(video, userVideoStream)
+        })
+      })
+
+      db.collection('users').onSnapshot(async (snapshot) => {
+        let data = snapshot.docs
+        data.map(doc => {
+            console.log("DOC IS ", doc.data())
+            if(ourId != doc.data().userId){
+              connectToNewUser(doc.data().userId,stream)
+
+            }
+            
+        })
+      })
+    
+     
+    })
+
+
+    // console.log("stream is", stream)
+
+
     myPeer.on("open", (id) => {
         console.log("my peer open", id);
+        setOurId(id)
+        console.log("setting our id to ", id)
     
         db.collection("users").add({ userId: id });
       });
 
-      myPeer.on("call", (call) => {
-        // console.log("do we make it here, if so STREAM IS", stream);
+      // myPeer.on("call", (call) => {
+      //   // console.log("do we make it here, if so STREAM IS", stream);
     
-        // console.log("do we make it here, if so CALL  IS", call.on);
+      //   // console.log("do we make it here, if so CALL  IS", call.on);
 
-        console.log("stream is ", stream)
-        call.answer(stream);
-        const video = document.createElement("video");
-        call.on("stream", (userVideoStream) => {
-            console.log(" ARE WE MKAING IT INTO THE STREAM HANDLER")
-          addVideoStream(video, userVideoStream);
-        });
-      });
+      //   console.log("stream is ", stream)
+      //   call.answer(stream);
+      //   const video = document.createElement("video");
+      //   call.on("stream", (userVideoStream) => {
+      //       console.log(" ARE WE MKAING IT INTO THE STREAM HANDLER")
+      //     addVideoStream(video, userVideoStream);
+      //   });
+      // });
     // // console.log("what is stream in useEffect", stream)
 
         
@@ -72,22 +110,7 @@ export default function GameRoom(props) {
 
         // 24
 
-    db.collection('users').onSnapshot(async (snapshot) => {
-        let data = snapshot.docs
-        data.map(doc => {
-            console.log("DOC IS ", doc.data())
-            connectToNewUser(doc.data().userId,stream)
-        })
-        // insert logic to check if a new user was added or deleted
-        // if data has increased in length we know we aded, otherwise we can use sets potentially to see which user was deleted and close their connection/remove them from peers
-        // // // console.log("IS THIS A THING", data[data.length-1].data())
-        // // // console.log("IS THIS A THING", stream)
-        
-        // else{
-        //     let userId = findDisconnect()
-        //     peers[userId].close()
-        // }
-    })
+    
         // // 33
         // // roomUserInfo = db.collection('users').doc(`${roomId}`);
     
@@ -97,10 +120,10 @@ export default function GameRoom(props) {
     //   })
   }, []); // use effect ends here
 
-  function connectToNewUser(userId, stream) {
+  async function connectToNewUser(userId, stream) {
 
 
-    
+    if(userId === ourId) return
     const call = myPeer.call(userId, stream);
 
     console.log("ARE WE SEEING CALLS", call)
@@ -110,49 +133,59 @@ export default function GameRoom(props) {
         return
     } 
     const video = document.createElement("video");
+
+    console.log("before stream event user: ", userId)
     call.on("stream", (userVideoStream) => {
-        console.log("made it into stream EVENT", userVideoStream)
-      addVideoStream(video, userVideoStream);
+      
+      if(userId != ourId ){
+        console.log("connecting to user: ", userId)
+        addVideoStream(video, userVideoStream);
+        setPeers({ ...peers, userId: call });
+      }
+      
     });
     call.on("close", () => {
       video.remove();
     });
-
     // peers[userId] = call
-
-    setPeers({ ...peers, userId: call });
+    
   }
 
-  function addVideoStream(video, stream) {
+  async function addVideoStream(video, stream) {
     // console.log("inside VideoStream", video);
     video.srcObject = stream;
-    video.addEventListener("loadedmetadata", () => {
+    await video.addEventListener("loadedmetadata", () => {
       video.play();
     });
-    // console.log("in add videoStream what is our new VIDEO ", video)
-    // console.log("in add videoStream what is our new VIDEO GRID ", videoGrid)
-    videoGrid.append(video);
+   
+    // videoGrid.append(video);
+    
+    setVideoList([...videoList,video])
+
+    console.log("what is video source in addVideoStream", video.srcObject)
+    
+  
+    
   }
-async function getStream(){
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
-      }).then(async newStream => {
-        addVideoStream(myVideo, newStream)
-        // // console.log("new stream is", newStream)
-        await setStream(newStream)
-        // // console.log("setting stream in state", stream)
-      })
-}
+// async function getStream(){
+//     navigator.mediaDevices.getUserMedia({
+//         video: true,
+//         audio: true
+//       }).then(async newStream => {
+//         console.log("setting stream in state1", newStream)
+//         addVideoStream(myVideo, newStream)
+//         // // console.log("new stream is", newStream)
+//         await setStream(newStream)
+//         console.log("setting stream in state", stream)
+//       })
+// }
 
   
 
-  return <div>hello</div>;
+  return (<div>
+            <VideoGrid videoList={videoList}></VideoGrid>
 
-  // return ( <div id="video-grid">
-  //             {videoGrid.map((video)  => {
-  //                 return video
-  //             })}
+          </div>)
 
-  //         </div>);
+ 
 }
---
+++ src/components/VideoGrid.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import Peer from "peerjs";
+import { db } from "../firebase/firebase";
+
+
+export default function VideoGrid({videoList}) {
+    if(videoList.length === 0){
+        return <div> loading</div>
+    }
+    
+    
+    let vid  = videoList[0]
+
+    console.log("what is vid.src object ", vid.srcObject)
+
+   
+
+
+    if(videoList.length === 0){
+        return <div> loading</div>
+    }
+   
+    return (
+        <div id="video-x">
+            <video srcObject={vid.srcObject}> </video>
+        </div>
+        
+    )
+    
+    
+
+}
