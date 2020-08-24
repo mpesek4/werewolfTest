37e4518ecf94364043c2a3d390979580e36a1edd
Michael Pesek <pesekm@dupage.edu>
2020-08-24 15:44:50 -0500

    night logic implemented, not tested

--- src/components/GameRoom.js
+++ src/components/GameRoom.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Peer from 'peerjs';
 import { db } from '../firebase/firebase';
-
 export default class GameRoom extends React.Component {
   constructor(props) {
     super(props);
@@ -21,7 +20,6 @@ export default class GameRoom extends React.Component {
     this.connectToNewUser = this.connectToNewUser.bind(this);
     this.addVideoStream = this.addVideoStream.bind(this);
   }
-
   componentDidMount() {
     navigator.mediaDevices
       .getUserMedia({
@@ -30,11 +28,9 @@ export default class GameRoom extends React.Component {
       })
       .then((stream) => {
         this.addVideoStream(this.state.myVideo, stream);
-
         this.state.myPeer.on('call', (call) => {
           call.answer(stream);
           const video = document.createElement('video');
-
           call.on('stream', (userVideoStream) => {
             if (!this.peers.has(call.peer)) {
               this.addVideoStream(video, userVideoStream);
@@ -42,7 +38,6 @@ export default class GameRoom extends React.Component {
             this.peers.add(call.peer);
           });
         });
-
         db.collection('users').onSnapshot(async (snapshot) => {
           let data = snapshot.docs;
           data.map((doc) => {
@@ -51,21 +46,27 @@ export default class GameRoom extends React.Component {
             }
           });
         });
+        //create firebase method to look up individual game
+        db.collection('gameState').doc(this.props.gameState.docid).onSnapshot(async (snapshot) => {
+          let game = snapshot.docs.data();
+          if (game.Night) {
+            handleNightLogic(game, ourId)
+          } else {
+            handleDayLogic(game, ourId)
+          }
+        })
       });
-
     this.state.myPeer.on('open', (id) => {
       this.setState({ ourId: id });
       db.collection('users').add({ userId: id });
     });
   }
-
   connectToNewUser(userId, stream) {
     const call = this.state.myPeer.call(userId, stream);
     if (!call) {
       return;
     }
     const video = document.createElement('video');
-
     call.on('stream', (userVideoStream) => {
       if (userId !== this.state.ourId) {
         if (!this.peers.has(call.peer)) {
@@ -77,7 +78,6 @@ export default class GameRoom extends React.Component {
       video.remove();
     });
   }
-
   addVideoStream(video, stream) {
     if (this.state.refCounter === 1) {
       this.videoRef1.current.srcObject = stream;
@@ -90,7 +90,35 @@ export default class GameRoom extends React.Component {
       this.setState({ refCounter: this.state.refCounter + 1 });
     }
   }
-
+  handleNightLogic(game, ourId) {
+    if (game.villagers.length === 0){
+      assignRoles(game)
+    }
+    if (game.checkWerewolf && game.checkSeer && game.checkMedic) {
+      if (game.werewolfChoice === game.medicChoice){
+        game.werewolfChoice = null
+      } else {
+        games.villagers = game.villagers.filter (villager => {
+          villager.id !== game.werewolfChoice
+        })
+        game.dead.push(game.werewolfChoice)
+        game.medicChoice = null
+        game.numVillagers = game.villagers.length-1
+        game.votesWerewolves = null
+        game.checkWerewolf = false
+        game.checkMedic = false
+        game.checkSeer = false
+      }
+    } //outer IF
+    else {
+      return
+    }
+    game.Night = false
+    //updating game state in DB
+    db.update(game)
+  }
+  handleDayLogic(game, ourId) {
+  }
   render() {
     return (
       <div>
@@ -101,12 +129,9 @@ export default class GameRoom extends React.Component {
     );
   }
 }
-
 // myPeer.on("call", (call) => {
 //   // console.log("do we make it here, if so STREAM IS", stream);
-
 //   // console.log("do we make it here, if so CALL  IS", call.on);
-
 //   console.log("stream is ", stream)
 //   call.answer(stream);
 //   const video = document.createElement("video");
@@ -116,16 +141,12 @@ export default class GameRoom extends React.Component {
 //   });
 // });
 // // console.log("what is stream in useEffect", stream)
-
 // socket.on('user-connected', userId => {
 //   connectToNewUser(userId, stream)
 // })
-
 // 24
-
 // // 33
 // // roomUserInfo = db.collection('users').doc(`${roomId}`);
-
 //   socket.on('user-disconnected', userId => {
 //     if (peers[userId]) peers[userId].close()
 //   })
